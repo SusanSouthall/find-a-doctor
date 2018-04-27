a1af612c4531003d044d3785e752e92bbbcb7443
Susan Southall <susan@Susans-MBP.hsd1.or.comcast.net>
2018-04-27 14:46:28 -0700

    Fix bad request API  call

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import './styles.css';
 
 $(document).ready(function() {
   $('#find').click(function() {
-    let doctors = $('#symptom-checker').val();
+    let symptoms = $('#symptom-checker').val();
     $('#symptom-checker').val("");
 
     let promise = new Promise(function(resolve, reject) {
       let doctorRequest = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/conditions?fields=toothache&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.523062%2C-122.676482%2C100&user_location=45.523062%2C-122.676482&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+
+      // let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.523062, -122.676482&limit=10&user_key=${process.env.exports.apiKey}`;
       doctorRequest.onload = function() {
         if (this.status === 200) {
           resolve(doctorRequest.response);
@@ -26,10 +28,11 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('#results').text(body);
+      $('#results').append("Call one of these doctors to help you" + "<br>" + body.data.name);
       // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
     }, function(error) {
       $('#results').text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
+// 45.523062, -122.676482
