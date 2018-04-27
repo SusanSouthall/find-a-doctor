b489542e4d19a84674573403f60c1c4ba037beca
Susan Southall <susan@Susans-MBP.hsd1.or.comcast.net>
2018-04-27 16:28:26 -0700

    Add doctor.js logic.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { } from "./business.js"
+import { Doctors } from "./doctor.js"
+import {} from "./symptom.js"
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,33 +7,45 @@ import './styles.css';
 
 $(document).ready(function() {
   $('#find').click(function() {
-    let symptoms = $('#symptom-checker').val();
+    let symptom = $('#symptom-checker').val();
     $('#symptom-checker').val("");
 
-    let promise = new Promise(function(resolve, reject) {
-      let doctorRequest = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.523062%2C-122.676482%2C100&user_location=45.523062%2C-122.676482&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+    let doctor = new Doctor();//instance to grab class from BL
+
+  let promise = doctor.stormGlassLogic(latInput, longInput);//run this instance on the method from the BL
+  promise.then(function(response){
+    response = JSON.parse(response); //cleans code
+    // console.log(response + "this is after the promise");
+    getElementsStorm(response, longInput, latInput); //calls on the function outside document ready function
+  }, function(Error) { //display error
+    console.log("Sorry, there is an Error loading your requested information!");
+  });
+
+
+    // let promise = new Promise(function(resolve, reject) {
+    //   let doctorRequest = new XMLHttpRequest();
+    //   let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.523062%2C-122.676482%2C100&user_location=45.523062%2C-122.676482&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
 
       // let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptoms}&location=45.523062, -122.676482&limit=10&user_key=${process.env.exports.apiKey}`;
-      doctorRequest.onload = function() {
-        if (this.status === 200) {
-          resolve(doctorRequest.response);
-          console.log(doctorRequest.response);
-        } else {
-          reject(Error(doctorRequest.statusText));
-        }
-      }
-      doctorRequest.open("GET", url, true);
-      doctorRequest.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('#results').append("Call one of these doctors to help you" + "<br>" + body.data.name);
-      // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('#results').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
+    //   doctorRequest.onload = function() {
+    //     if (this.status === 200) {
+    //       resolve(doctorRequest.response);
+    //       console.log(doctorRequest.response);
+    //     } else {
+    //       reject(Error(doctorRequest.statusText));
+    //     }
+    //   }
+    //   doctorRequest.open("GET", url, true);
+    //   doctorRequest.send();
+    // });
+    //
+    // promise.then(function(response) {
+    //   let body = JSON.parse(response);
+    //   $('#results').append("Call one of these doctors to help you" + "<br>" + body.data.name);
+    //   // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    // }, function(error) {
+    //   $('#results').text(`There was an error processing your request: ${error.message}`);
+  //   // });
+   });
 });
 // 45.523062, -122.676482
